032de8736ea4d453f1d8c9440b81c11cf960697c
chenxiaocehng <583188551@qq.com>
2019-09-11 13:37:00 +0800

    分模块梳理

--- producer-demo/src/main/java/com/linjingc/producerdemo/ProducerDemoApplication.java
+++ producer-demo/src/main/java/com/linjingc/producerdemo/ProducerDemoApplication.java
@@ -1,9 +1,11 @@
 package com.linjingc.producerdemo;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubbo
 public class ProducerDemoApplication {
 
 	public static void main(String[] args) {
